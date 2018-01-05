3d76274ba9ec8a7205b36dd258ecdbce9b806530
sapsztor <sapsztor@gmail.com>
2018-01-05 22:30:04 +0100

    All TODO completed.

---
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -0,0 +1,33 @@
+package com.example.android.android_me.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+
+public class BodyPartFragment extends Fragment {
+    
+    
+    public BodyPartFragment() {
+        // Required empty public constructor
+    }
+    
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
+        imageView.setImageResource(AndroidImageAssets.getHeads().get(0));
+        
+        return rootView;
+    }
+    
+}
