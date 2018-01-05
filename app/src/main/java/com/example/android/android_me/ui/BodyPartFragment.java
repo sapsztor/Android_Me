f992585945718beafe3d1667b40139e98f679e63
sapsztor <sapsztor@gmail.com>
2018-01-05 23:48:48 +0100

    Minor changes

--- app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -15,6 +15,7 @@ import com.example.android.android_me.data.AndroidImageAssets;
 
 public class BodyPartFragment extends Fragment {
     
+    View rootView;
     
     public BodyPartFragment() {
         // Required empty public constructor
@@ -23,11 +24,17 @@ public class BodyPartFragment extends Fragment {
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
+        rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
         ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
         imageView.setImageResource(AndroidImageAssets.getHeads().get(0));
         
         return rootView;
+    }
+    
+    public void setBodyPart(int bp) {
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
+        imageView.setImageResource(AndroidImageAssets.getHeads().get(bp));
+        
     }
     
 }
